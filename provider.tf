91bd2c9ff1c1bee573398d38d2b5da8c4797bd88
Pawankumarmeena425 <pawankuamrmeena425@gmail.com>
2024-09-05 18:03:37 +0530

    modifed provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region     = var.region
+  region =  "us-east-1"
+  access_key = var.access_key
+  secret_key =  var.secret_key
+}
+
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
 }
